e03504be5abd6a4e998ab3bae5c242404fb22cd2
Jukkis <jukka-pekka.lappalainen@outlook.com>
2020-02-29 19:47:51 +0200

    Uusi about

--- sigMap.js
+++ sigMap.js
@@ -7,21 +7,15 @@ const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl,{attribution});
 tiles.addTo(mymap);
 
-var marker = L.marker([60.223970, 24.75809]).addTo(mymap);
-marker.bindPopup("<b>The home of Sigma</b>").openPopup();
-
-var circle = L.circle([60.223970, 24.75809], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 50
-}).addTo(mymap);
-
 const myIcon = L.icon({
     iconUrl: "sigma-logo.png",
-    iconSize: [40, 95],
+    iconSize: [30, 65],
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76],
 
 });
-L.marker([60.223970, 24.75809], {icon: myIcon}).addTo(mymap);
+var marker = L.marker([60.223970, 24.75809], {icon: myIcon}).addTo(mymap);
+marker.bindPopup("<b>The home of Sigma</b>").openPopup();
+
+
+

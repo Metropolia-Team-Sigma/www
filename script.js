a38f90f785387763d92a967c2b94f44fc0da085c
Linus Willner <hello@linuswillner.me>
2020-03-09 19:02:25 +0200

    Rework home page and restructure project

--- script.js
+++
@@ -1,7 +0,0 @@
-
-const navWrapper = document.querySelector('.nav-wrapper')
-const navToogler = document.querySelector('.nav-toogler')
-
-navToogler.addEventListener('click', function (event) {
-  navWrapper.classList.toggle('active')
-})
